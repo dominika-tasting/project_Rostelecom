1a2c3cf8b5affede167fe8f52441f86b15c08053
dominika-tasting <k.or121@mail.ru>
2023-04-29 14:07:42 +0400

    project_Rostelecom

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import time
+import pytest
+from selenium import webdriver
+from faker import Faker
+
+
+@pytest.fixture(autouse=True)
+def browser():
+    driver = webdriver.Chrome(executable_path='chrome.exe')
+
+    yield driver
+    driver.quit()
+    
